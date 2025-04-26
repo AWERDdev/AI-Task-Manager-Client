09ab45b263029981a51f3fc32b7f27d77bad4430
AWERDdev <kokorami565@gmail.com>
2025-04-26 13:11:02 +0300

    Updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import React from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [
-    React(),
+    react(),
     tailwindcss(),
+
   ],
 })
